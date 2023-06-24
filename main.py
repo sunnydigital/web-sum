37d6d0b777bae11d0cf43532f072677bacd8655a
Sunny Son <74517869+sunnydigital@users.noreply.github.com>
2023-06-24 02:24:15 -0400

    CHanges to yaml, py, and json files

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import json
+import requests
+from bs4 import BeautifulSoup
 
 import quart
 import quart_cors
@@ -6,10 +8,14 @@ from quart import request
 
 app = quart_cors.cors(quart.Quart(__name__), allow_origin='https://chat.openai.com')
 
-
+async def get_links(url):
+    response = await requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    links = [a['href'] for a in soup.find_all('a', href=True)]
+    return links
 
 def main():
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5003)
 
 if __name__ == '__main__':
     main()
